4632458520d199860d176257e96419d813d07c91
shakystar <shakystar777@gmail.com>
2024-05-13 18:52:15 +0900

    asd

--- boot-qemu.sh
+++ boot-qemu.sh
@@ -1,4 +1,4 @@
-qemu-system-arm -s -S -cpu cortex-a7 \
+qemu-system-arm  -cpu cortex-a7 \
 -machine virt -nographic -m 512M \
 -serial pty -bios u-boot/u-boot.bin \
 -netdev user,id=net0,tftp=/tftpboot/,hostfwd=tcp::2222-:22 \
